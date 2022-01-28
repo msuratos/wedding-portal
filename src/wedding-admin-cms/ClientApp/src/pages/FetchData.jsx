c979e29f5f66fcd03d82ade3b2b1be010fd4b605
Melvin Suratos <melvinsuratos@hotmail.com>
2022-01-27 20:34:14 -0800

    Added role tab and respective inputs

--- src/wedding-admin-cms/ClientApp/src/pages/FetchData.jsx
+++ src/wedding-admin-cms/ClientApp/src/pages/FetchData.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { Col, Row } from 'reactstrap';
+import {
+  Col, Nav, NavItem, NavLink, Row,
+  TabContent, TabPane, Form, FormGroup,
+  Label, Input, Button, Card, CardBody, Table
+} from 'reactstrap';
 
 const FetchData = () => {
-  const [loading, setLoading] = useState(false);
-  const [wedding, setWedding] = useState({});
+  const [activeTab, setActiveTab] = useState('1');
   const [bride, setBride] = useState('');
-  const [groom, setGroom] = useState('');
   const [ceremonyDate, setCeremonyDate] = useState(undefined);
+  const [groom, setGroom] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [roles, setRoles] = useState([]);
+  const [wedding, setWedding] = useState({});
 
   const createWedding = async () => {
     setLoading(true);
@@ -46,37 +52,98 @@ const FetchData = () => {
   };
 
   return (
-    <>
-      {
-        loading
-          ? <p><em>Loading...</em></p>
-          : (
-            <div>
-              <h1 id="tabelLabel">Wedding</h1>
-              <p>This component demonstrates fetching data from the server.</p>
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <Row>
-                  <Col md={2}><label htmlFor="bride">Bride</label></Col>
-                  <Col md={10}><input type="text" id="bride" value={bride} onChange={e => setBride(e.target.value)} /></Col>
-                </Row>
-                <Row>
-                  <Col md={2}><label htmlFor="groom">Groom</label></Col>
-                  <Col md={10}><input type="text" id="groom" value={groom} onChange={e => setGroom(e.target.value)} /></Col>
-                </Row>
-                <Row>
-                  <Col md={2}><label htmlFor="date">Cremony Date</label></Col>
-                  <Col md={10}><input type="datetime-local" id="date" value={ceremonyDate} onChange={e => setCeremonyDate(e.target.value)} /></Col>
-                </Row>
-              </div>
-              <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '15px' }}>
-                <input type="submit" className="btn btn-primary" value="Create Wedding" onClick={createWedding} />
-                <input type="button" className="btn btn-secondary" value="Get Wedding" />
-              </div>
-              <WeddingTable wedding={wedding} />
-            </div>
-          )
-      }
-    </>
+    <div>
+      <Nav tabs>
+        <NavItem>
+          <NavLink active={activeTab === '1'} onClick={() => setActiveTab('1')}>Edit Wedding</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink active={activeTab === '2'} onClick={() => setActiveTab('2')}>Edit Roles</NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+          {
+            loading
+              ? <p><em>Loading...</em></p>
+              : (
+                <div>
+                  <h1 id="tabelLabel">Wedding</h1>
+                  <p>This component demonstrates fetching data from the server.</p>
+                  <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Row>
+                      <Col md={2}><label htmlFor="bride">Bride</label></Col>
+                      <Col md={10}><input type="text" id="bride" value={bride} onChange={e => setBride(e.target.value)} /></Col>
+                    </Row>
+                    <Row>
+                      <Col md={2}><label htmlFor="groom">Groom</label></Col>
+                      <Col md={10}><input type="text" id="groom" value={groom} onChange={e => setGroom(e.target.value)} /></Col>
+                    </Row>
+                    <Row>
+                      <Col md={2}><label htmlFor="date">Cremony Date</label></Col>
+                      <Col md={10}><input type="datetime-local" id="date" value={ceremonyDate} onChange={e => setCeremonyDate(e.target.value)} /></Col>
+                    </Row>
+                  </div>
+                  <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '15px' }}>
+                    <input type="submit" className="btn btn-primary" value="Create Wedding" onClick={createWedding} />
+                    <input type="button" className="btn btn-secondary" value="Get Wedding" />
+                  </div>
+                  <WeddingTable wedding={wedding} />
+                </div>
+              )
+          }
+        </TabPane>
+        <TabPane tabId="2">
+          <Card>
+            <CardBody>
+              <Row>
+                <Col sm={12}>
+                  <Form>
+                    <FormGroup row>
+                      <Label for="role-name" sm={2}>Role Name</Label>
+                      <Col sm={10}>
+                        <Input id="role-name" name="name" placeholder="Name of role" type="text" />
+                      </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                      <Label for="role-desc" sm={2}>Description</Label>
+                      <Col sm={10}>
+                        <Input id="role-desc" name="description" placeholder="Describe the role" type="textarea" />
+                      </Col>
+                    </FormGroup>
+                    <Row>
+                      <Col sm={6}><Button style={{ width: '100%' }} type='submit' color='primary'>Submit</Button></Col>
+                      <Col sm={6}><Button style={{ width: '100%' }} color='secondary'>Cancel</Button></Col>
+                    </Row>
+                  </Form>
+                </Col>
+              </Row>
+              <Row style={{marginTop: '15px'}}>
+                <Col sm={12}>
+                  <Table className='table table-striped'>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {roles.map(el => (
+                        <tr key={el.id}>
+                          <td scope="row">{el.name}</td>
+                          <td scope="row">{el.description}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </TabPane>
+      </TabContent>
+    </div>
   );
 };
 
