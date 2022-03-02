510c537295de288304e343c914a51cb521657438
Melvin Suratos <melvinsuratos@hotmail.com>
2022-03-01 22:03:58 -0800

    started on Azure B2C signin

--- src/wedding-admin-cms/ClientApp/src/App.jsx
+++ src/wedding-admin-cms/ClientApp/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { useMsal } from "@azure/msal-react";
+
 import { Layout } from './components/Layout';
 import Home from './pages/Home';
 import EditWedding from './pages/EditWedding';
@@ -7,9 +9,13 @@ import EditWedding from './pages/EditWedding';
 import './custom.css'
 
 const App = () => {
+  const msal = useMsal();
+  const username = msal.accounts[0].username;
+
   return (
     <Layout>
       <Switch>
+        Hello, {username}!
         <Route exact path='/' component={Home} />
         <Route path='/edit-wedding' component={EditWedding} />
         <Route path="*" component={() => <div>Page not found! Try again!</div>} />
--- src/wedding-admin-cms/ClientApp/src/index.js
+++ src/wedding-admin-cms/ClientApp/src/index.js
@@ -1,14 +1,44 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
+import { PublicClientApplication } from '@azure/msal-browser';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, MsalProvider, useMsal, useIsAuthenticated } from "@azure/msal-react";
+import { msalConfig, b2cPolicies } from "./utils/authConfig";
+
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+const msalInstance = new PublicClientApplication(msalConfig);
+
+const Login = () => {
+  const msal = useMsal();
+  const isAuthenticated = useIsAuthenticated();
+
+  useEffect(() => {
+    if (!isAuthenticated)
+      msal.instance.loginRedirect(b2cPolicies.authorities.signUpSignIn);
+  }, []);
+
+  return (
+    <div>
+      <h3>Login</h3>
+      <p>Redirecting you to login with Azure B2C</p>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <AuthenticatedTemplate>
+        <App />
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <Login />
+      </UnauthenticatedTemplate>
+    </MsalProvider>
   </BrowserRouter>,
   rootElement);
