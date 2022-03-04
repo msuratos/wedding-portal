4691beea6591cdba2598b0f4b82a4dd0cf8e45a0
Melvin Suratos <melvinsuratos@hotmail.com>
2022-03-03 23:20:08 -0800

    included the access token when calling the web api

--- src/wedding-admin-cms/ClientApp/src/apis/roleApi.js
+++ src/wedding-admin-cms/ClientApp/src/apis/roleApi.js
@@ -1,10 +1,20 @@
-export const addRole = async (role) => {
-  const requestOptions = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(role) };
+export const addRole = async ({description, name, token}) => {
+  const requestOptions = { method: 'POST', body: JSON.stringify({description, name}), 
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    } 
+  };
+
   const resp = await fetch('api/role', requestOptions);
   return await resp.json();
 };
 
-export const getRoles = async () => {
-  const resp = await fetch('api/role');
+export const getRoles = async (accesstoken) => {
+  const resp = await fetch('api/role', {
+    headers: {
+      "Authorization": `Bearer ${accesstoken}`
+    }
+  });
   return await resp.json();
 };
