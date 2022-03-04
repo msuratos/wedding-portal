b72e5185e5e8b7c5d149bc361ccffb6d10fda3ad
Melvin Suratos <melvinsuratos@hotmail.com>
2022-03-03 22:16:36 -0800

    put username/displayname in proper navmenu

--- src/wedding-admin-cms/ClientApp/src/App.jsx
+++ src/wedding-admin-cms/ClientApp/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { useMsal } from "@azure/msal-react";
 
 import { Layout } from './components/Layout';
 import Home from './pages/Home';
@@ -9,13 +8,9 @@ import EditWedding from './pages/EditWedding';
 import './custom.css'
 
 const App = () => {
-  const msal = useMsal();
-  const username = msal.accounts[0].username;
-
   return (
     <Layout>
       <Switch>
-        Hello, {username}!
         <Route exact path='/' component={Home} />
         <Route path='/edit-wedding' component={EditWedding} />
         <Route path="*" component={() => <div>Page not found! Try again!</div>} />
