d9d94f5ca0eec4a3d2d4ee9cc669bc8246a7f414
Melvin Suratos <melvinsuratos@hotmail.com>
2022-03-06 14:58:27 -0800

    implemented adding entourage logic

--- src/wedding-admin-cms/ClientApp/src/apis/weddingApi.js
+++ src/wedding-admin-cms/ClientApp/src/apis/weddingApi.js
@@ -1,8 +1,20 @@
+export const addEntourage = async (request) => {
+  const { name, roleIdOfEntourage, entourageOfWeddingId, token } = request;
+
+  const resp = await fetch('wedding/entourage', {
+    method: 'POST', body: JSON.stringify({ name, roleIdOfEntourage, entourageOfWeddingId }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  return await resp.json();
+};
+
 export const createWedding = async (request, tokenCache) => {
   const resp = await fetch('wedding', {
     method: 'POST', body: JSON.stringify(request),
-    headers:
-    {
+    headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${tokenCache.accessToken}`
     }
