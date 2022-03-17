f025959d1526d095badcbfb041b7004e81016cb4
Melvin Suratos <melvinsuratos@hotmail.com>
2022-03-16 21:02:09 -0700

    fixed issue with unicode-bom error

--- src/wedding-frontend/ClientApp/src/App.jsx
+++ src/wedding-frontend/ClientApp/src/App.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Grid, Typography } from '@mui/material';
 
 import weddingPic from './assets/wedding.jpeg';
