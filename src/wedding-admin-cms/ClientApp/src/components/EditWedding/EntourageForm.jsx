d9bf4809d4717c8fe0e6f4b11830c1d7825b39a9
Melvin Suratos <melvinsuratos@hotmail.com>
2022-03-27 22:23:44 -0700

    moved main edit entourage to another component

---
+++ src/wedding-admin-cms/ClientApp/src/components/EditWedding/EntourageForm.jsx
@@ -0,0 +1,122 @@
+﻿import React, { useEffect, useMemo, useState } from 'react';
+import {
+  Button, Col, Dropdown, DropdownItem, DropdownMenu,
+  DropdownToggle, Form, FormGroup, Input, Label, Row, Table
+} from 'reactstrap';
+import { useMsal } from '@azure/msal-react';
+
+import { addEntourage } from '../../apis/weddingApi';
+import { getRoles } from '../../apis/roleApi';
+
+const EntourageForm = (props) => {
+  const { setErrorShowAlert, setSuccessShowAlert, wedding } = props;
+
+  const [entourage, setEntourage] = useState([]);
+  const [entourageName, setEntourageName] = useState('');
+  const [entourageRole, setEntourageRole] = useState(0);
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const [roles, setRoles] = useState([]);
+
+  const msal = useMsal();
+  const { instance, accounts } = msal;
+
+  const silentRequest = useMemo(() => {
+    return {
+      scopes: ["https://syzmicb2c.onmicrosoft.com/weddingportalapi/user.access"],
+      account: accounts[0]
+    }
+  }, [accounts]);
+
+  const createEntourage = async (e) => {
+    e.preventDefault();
+
+    try {
+      const tokenCache = await instance.acquireTokenSilent(silentRequest);
+      const newEntourage = await addEntourage(
+        {
+          name: entourageName, roleIdOfEntourage: entourageRole,
+          entourageOfWeddingId: wedding.weddingId, token: tokenCache.accessToken
+        }
+      );
+
+      setSuccessShowAlert(true);
+      setTimeout(() => setSuccessShowAlert(false), 3000);
+
+      setEntourage([...entourage, newEntourage]);
+    }
+    catch (error) {
+      console.error(error);
+      setErrorShowAlert(true);
+      setTimeout(() => setErrorShowAlert(false), 3000);
+    }
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      const tokenCache = await instance.acquireTokenSilent(silentRequest);
+      const rolesResp = await getRoles(tokenCache.accessToken);
+
+      setRoles(rolesResp);
+    };
+
+    init();
+  }, []);
+
+  return (
+    <>
+      <Row style={{ marginTop: '15px' }}>
+        <Col sm={12}>
+          <Form onSubmit={createEntourage}>
+            <FormGroup row>
+              <Label for="name" sm={2}>Entourage Name</Label>
+              <Col sm={10}>
+                <Input id="name" name="name" placeholder="Name of entourage" type="text" value={entourageName} onChange={e => setEntourageName(e.target.value)} />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="entourage-role" sm={2}>Entourage Role</Label>
+              <Col sm={10}>
+                <Dropdown toggle={() => setOpenDropdown(!openDropdown)} isOpen={openDropdown}>
+                  <DropdownToggle caret outline>
+                    {entourageRole !== 0 ? entourageRole : 'Assign a role'}
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    {roles.map((el, i) => {
+                      return <DropdownItem key={`role-item-${i}`} onClick={() => setEntourageRole(el.id)}>{el.name}</DropdownItem>
+                    })}
+                  </DropdownMenu>
+                </Dropdown>
+              </Col>
+            </FormGroup>
+            <Row>
+              <Col sm={6}><Button style={{ width: '100%' }} type='submit' color='primary'>Submit</Button></Col>
+              <Col sm={6}><Button style={{ width: '100%' }} color='secondary'>Cancel</Button></Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: '15px' }}>
+        <Col sm={12}>
+          <Table className='table table-striped'>
+            <thead>
+              <tr>
+                <th scope="row">Name</th>
+                <th scope="row">Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              {entourage.map(el => (
+                <tr key={el.entourageId}>
+                  <td>{el.name}</td>
+                  <td>{el.roleIdOfEntourage}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default EntourageForm;
--- src/wedding-admin-cms/ClientApp/src/pages/EditWedding.jsx
+++ src/wedding-admin-cms/ClientApp/src/pages/EditWedding.jsx
@@ -1,26 +1,21 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import {
-  Col, Dropdown, DropdownItem, DropdownMenu,
-  DropdownToggle, Nav, NavItem, NavLink, Row,
-  TabContent, TabPane, FormGroup, UncontrolledAlert,
-  Label, Input, Button, Table, Form
+  Nav, NavItem, NavLink,
+  TabContent, TabPane, UncontrolledAlert
 } from 'reactstrap';
 import { useMsal } from '@azure/msal-react';
 
-import { addEntourage, getWedding } from '../apis/weddingApi';
+import { getWedding } from '../apis/weddingApi';
 import MessageForm from '../components/EditWedding/MessageForm';
 import RoleForm from '../components/EditWedding/RoleForm';
 import WeddingForm from '../components/EditWedding/WeddingForm';
+import EntourageForm from '../components/EditWedding/EntourageForm';
 
 const EditWedding = () => {
   const [activeTab, setActiveTab] = useState('1');
-  const [entourage, setEntourage] = useState([]);
-  const [entourageName, setEntourageName] = useState('');
-  const [entourageRole, setEntourageRole] = useState(0);
-  const [openDropdown, setOpenDropdown] = useState(false);
+  
   const [showSuccessAlert, setSuccessShowAlert] = useState(false);
   const [showErrorAlert, setErrorShowAlert] = useState(false);
-  const [roles] = useState([]);
 
   const [wedding, setWedding] = useState({
     ceremonyDate: new Date(), messageToEveryone: '', receptionDate: new Date()
@@ -35,30 +30,6 @@ const EditWedding = () => {
       account: accounts[0]
     }
   }, [accounts]);
-
-  const createEntourage = async (e) => {
-    e.preventDefault();
-
-    try {
-      const tokenCache = await instance.acquireTokenSilent(silentRequest);
-      const newEntourage = await addEntourage(
-        {
-          name: entourageName, roleIdOfEntourage: entourageRole,
-          entourageOfWeddingId: wedding.weddingId, token: tokenCache.accessToken
-        }
-      );
-
-      setSuccessShowAlert(true);
-      setTimeout(() => setSuccessShowAlert(false), 3000);
-
-      setEntourage([...entourage, newEntourage]);
-    }
-    catch (error) {
-      console.error(error);
-      setErrorShowAlert(true);
-      setTimeout(() => setErrorShowAlert(false), 3000);
-    }
-  };
 
   useEffect(() => {
     const init = async () => {
@@ -101,57 +72,7 @@ const EditWedding = () => {
           <RoleForm setErrorShowAlert={setErrorShowAlert} setSuccessShowAlert={setSuccessShowAlert} />
         </TabPane>
         <TabPane tabId="4">
-          <Row style={{ marginTop: '15px' }}>
-            <Col sm={12}>
-              <Form onSubmit={createEntourage}>
-                <FormGroup row>
-                  <Label for="name" sm={2}>Entourage Name</Label>
-                  <Col sm={10}>
-                    <Input id="name" name="name" placeholder="Name of entourage" type="text" value={entourageName} onChange={e => setEntourageName(e.target.value)} />
-                  </Col>
-                </FormGroup>
-                <FormGroup row>
-                  <Label for="entourage-role" sm={2}>Entourage Role</Label>
-                  <Col sm={10}>
-                    <Dropdown toggle={() => setOpenDropdown(!openDropdown)} isOpen={openDropdown}>
-                      <DropdownToggle caret outline>
-                        {entourageRole !== 0 ? entourageRole : 'Assign a role'}
-                      </DropdownToggle>
-                      <DropdownMenu>
-                        {roles.map((el, i) => {
-                          return <DropdownItem key={`role-item-${i}`} onClick={() => setEntourageRole(el.id)}>{el.name}</DropdownItem>
-                        })}
-                      </DropdownMenu>
-                    </Dropdown>
-                  </Col>
-                </FormGroup>
-                <Row>
-                  <Col sm={6}><Button style={{ width: '100%' }} type='submit' color='primary'>Submit</Button></Col>
-                  <Col sm={6}><Button style={{ width: '100%' }} color='secondary'>Cancel</Button></Col>
-                </Row>
-              </Form>
-            </Col>
-          </Row>
-          <Row style={{ marginTop: '15px' }}>
-            <Col sm={12}>
-              <Table className='table table-striped'>
-                <thead>
-                  <tr>
-                    <th scope="row">Name</th>
-                    <th scope="row">Role</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {entourage.map(el => (
-                    <tr key={el.entourageId}>
-                      <td>{el.name}</td>
-                      <td>{el.roleIdOfEntourage}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Col>
-          </Row>
+          <EntourageForm wedding={wedding} setErrorShowAlert={setErrorShowAlert} setSuccessShowAlert={setSuccessShowAlert} />
         </TabPane>
       </TabContent>
     </div>
