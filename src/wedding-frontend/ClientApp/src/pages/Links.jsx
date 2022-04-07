9ff90a3ba3c35dc5e85eb4e2ac72d991d3f070bc
Melvin Suratos <melvinsuratos@hotmail.com>
2022-04-06 22:17:01 -0700

    added basic passphrase logic

--- src/wedding-frontend/ClientApp/src/pages/Links.jsx
+++ src/wedding-frontend/ClientApp/src/pages/Links.jsx
@@ -1,8 +1,48 @@
-﻿import React from 'react';
+﻿import React, { useEffect, useState } from 'react';
+import { Button, Grid } from '@mui/material';
 
 const Links = () => {
+  const [isValidPassphrase, setIsValidPassphrase] = useState(false);
+
+  useEffect(() => {
+    function isExistingValidPassphrase () {
+      // check local storage
+      if (localStorage.getItem('validPassphrase'))
+        setIsValidPassphrase(true);
+    };
+
+    isExistingValidPassphrase();
+  }, []);
+
   return (
-    <div>Links</div>
+    <>
+      {
+        !isValidPassphrase
+          ? (
+            <Grid container sx={{p: '15px'}}>
+              <Grid item xs={12} md={6} sx={{textAlign: 'center'}}>
+                <label htmlFor="passphrase">Passphrase</label>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <input type="text" id="passphrase" title="passphrase" style={{ width: '100%' }} />
+              </Grid>
+              <Grid item xs={12} md={12} sx={{textAlign: 'center'}}>
+                <Button variant="contained" color="primary">Submit</Button>
+              </Grid>
+            </Grid>
+          )
+          : (
+            <div>
+              <ul>
+                <li><a href="info">Info</a></li>
+                <li><a href="rsvp">RSVP</a></li>
+                <li><a href="faq">FAQs</a></li>
+                <li><a href="contact">Contact Us</a></li>
+              </ul>
+            </div>
+          )
+      }
+    </>
   );
 };
 
