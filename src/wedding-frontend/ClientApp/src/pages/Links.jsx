6dfeb676d27e1bca5b4199f4d47be3aa901588fb
Melvin Suratos <melvinsuratos@hotmail.com>
2022-04-20 22:09:44 -0700

    fixing file format bom warning

--- src/wedding-frontend/ClientApp/src/pages/Links.jsx
+++ src/wedding-frontend/ClientApp/src/pages/Links.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   Button, Grid, Input, Card,
