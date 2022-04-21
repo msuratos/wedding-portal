72477f160deeac37bd273b77a4c49d7333d5b4c0
Melvin Suratos <melvinsuratos@hotmail.com>
2022-04-20 22:29:28 -0700

    trying different unicode html value for the star symbol

--- src/wedding-frontend/ClientApp/src/components/Background.jsx
+++ src/wedding-frontend/ClientApp/src/components/Background.jsx
@@ -56,7 +56,7 @@ const Background = () => {
 		div.style.left = `${Math.round(Math.random() * 100)}%`;
 		div.style.width = div.style.height = `${Math.random() * 5 + 12}px`;
 		div.style.color = Math.round(Math.random() * 2) === 1 ? 'white' : blue[200];
-		div.innerHTML = '&#128964;';
+		div.innerHTML = '&#x1F7C4;';
 
 		star.current.appendChild(div);
 	};
