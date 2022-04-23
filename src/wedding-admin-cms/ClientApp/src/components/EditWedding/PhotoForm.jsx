d42bfaebb06faf2386fdc94e77214948132b52d7
Melvin Suratos <melvinsuratos@hotmail.com>
2022-04-23 12:37:47 -0700

    added photos database & initial implementation of uploading photos

---
+++ src/wedding-admin-cms/ClientApp/src/components/EditWedding/PhotoForm.jsx
@@ -0,0 +1,76 @@
+﻿import React, { useEffect, useMemo, useState } from 'react';
+import {
+  Button, Col, Form, FormGroup,
+  Input, Label, Row, Table
+} from 'reactstrap';
+import { useMsal } from '@azure/msal-react';
+
+const PhotoForm = (props) => {
+  const { setErrorShowAlert, setSuccessShowAlert, wedding } = props;
+  const [files, setFiles] = useState([]);
+  const [fileValues, setFileValues] = useState('');
+  const [loading, setLoading] = useState(false);
+  const msal = useMsal();
+  const { instance, accounts } = msal;
+
+  const silentRequest = useMemo(() => {
+    return {
+      scopes: ["https://syzmicb2c.onmicrosoft.com/weddingportalapi/user.access"],
+      account: accounts[0]
+    }
+  }, [accounts]);
+
+  const addPhotos = async (e) => {
+    const tokenCache = await instance.acquireTokenSilent(silentRequest);
+
+    e.preventDefault();
+    setLoading(true);
+
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++)
+      data.append("data", files[i]);
+
+    const resp = await fetch(`/Wedding/Photos?weddingId=${wedding.weddingId}&forPage=About Us`, {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Authorization': `Bearer ${tokenCache.accessToken}`
+      }
+    });
+
+    if (resp.ok)
+      setSuccessShowAlert(true);
+    else
+      setErrorShowAlert(false);
+
+    setLoading(false);
+    setFiles([]);
+    setFileValues('');
+  };
+
+  return (
+    <>
+      {loading && <div>Loading...</div>} 
+      <Row style={{ marginTop: '15px' }}>
+        <Col sm={12}>
+          <FormGroup row>
+            <Label for="photos" sm={2}>Photos</Label>
+            <Col sm={10}>
+              <Input id="photos" name="photos" placeholder="Add Photos" type="file" multiple value={fileValues}
+                onChange={e => {
+                  setFiles(e.target.files);
+                  setFileValues(e.target.value);
+                }} />
+            </Col>
+          </FormGroup>
+          <Row>
+            <Col sm={6}><Button block type='submit' color='primary' onClick={addPhotos}>Submit</Button></Col>
+            <Col sm={6}><Button block color='secondary'>Cancel</Button></Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default PhotoForm;
---
+++ src/wedding-admin-cms/Persistance/Migrations/20220423174013_AddPhotos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace wedding_admin_cms.Persistance.Migrations
+{
+    public partial class AddPhotos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    PhotoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    FileName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FileType = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ForPage = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    FkWeddingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.PhotoId);
+                    table.ForeignKey(
+                        name: "FK_Photos_Weddings_FkWeddingId",
+                        column: x => x.FkWeddingId,
+                        principalTable: "Weddings",
+                        principalColumn: "WeddingId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_FkWeddingId",
+                table: "Photos",
+                column: "FkWeddingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
