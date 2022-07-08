665cab67042fb0db0cddea18978b0784ac6d99b0
Melvin Suratos <72821450+msuratos@users.noreply.github.com>
2022-07-08 08:25:11 -0700

    implemented close trivia (#67)

---
+++ src/wedding-admin-cms/Dtos/CloseTriviaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace wedding_admin_cms.Dtos
+{
+  public class CloseTriviaDto
+  {
+    public Guid WeddingId { get; set; }
+    public Guid TriviaId { get; set; }
+    public bool Status { get; set; }
+  }
+}
