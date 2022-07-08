74bb31800a242641ab4b72fe199081a207d4b443
Melvin Suratos <melvinsuratos@hotmail.com>
2022-07-08 08:07:15 -0700

    Implemented adding trivia user answers

--- src/wedding-frontend/ClientApp/src/pages/Trivia.jsx
+++ src/wedding-frontend/ClientApp/src/pages/Trivia.jsx
@@ -1,27 +1,62 @@
 import React, { useEffect, useState } from 'react';
+import SwipeableViews from 'react-swipeable-views';
 
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
+import MobileStepper from '@mui/material/MobileStepper';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+
 import ListPageLayout from '../components/ListPageLayout';
-import { getTrivia } from '../apis/triviaApi';
+import { getTrivia, postAnswer } from '../apis/triviaApi';
 
 const Trivia = () => {
+  const [completedForm, setCompletedForm] = useState(false);
   const [canSeeQuestions, setCanSeeQuestions] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [username, setUsername] = useState('');
   const [trivia, setTrivia] = useState({});
 
-  console.log(trivia);
+  // state for mobilestepper, mobile view
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
 
   const onUsernameButtonClick = () => {
     setUsername(username);
     localStorage.setItem('username', username);
 
     setCanSeeQuestions(true);
+  };
+
+  const onHimHerButtonClick = async (name, triviaQuestionId) => {
+    const resp = await postAnswer({ username, userAnswer: name, triviaQuestionId });
+
+    if (resp.ok) {
+      setActiveStep(activeStep + 1);
+      localStorage.setItem('completed', true);
+    }
+    else {
+      alert('Something went wrong, please try again');
+      console.error(resp.statusText, await resp.text());
+    }
+
+    if (activeStep + 1 === questions.length)
+      setCompletedForm(true);
   };
 
   useEffect(() => {
@@ -34,6 +69,11 @@ const Trivia = () => {
       if (username !== null && username !== '') {
         setUsername(username);
         setCanSeeQuestions(true);
+      }
+
+      const completed = localStorage.getItem('completed');
+      if (completed !== null && completed !== '') {
+        setCompletedForm(completed);
       }
     };
 
@@ -48,21 +88,42 @@ const Trivia = () => {
       </Grid>
       <Grid item xs={12} sx={{ pt: '10px' }}>
         {
-          !canSeeQuestions
-            ? (
-              <>
-                <TextField label='Full Name' sx={{ mb: '5px' }} value={username} onChange={(e) => setUsername(e.target.value)} fullWidth />
-                <Button color='primary' variant='contained' onClick={onUsernameButtonClick} fullWidth>Submit</Button>
-              </>
-            )
-            : (
-              <ul>
-                {/* TODO: switch to swipeable views for each question */}
-                {
-                  questions.map(question => <li key={question.triviaQuestionId}>{question.question}</li>)
-                }
-              </ul>
-            )
+          completedForm ? <Typography variant='h6' gutterBotom>You have submitted already, no retries 😊</Typography>
+            :
+            !canSeeQuestions
+              ? (
+                <>
+                  <TextField label='Full Name' sx={{ mb: '5px' }} value={username} onChange={(e) => setUsername(e.target.value)} fullWidth />
+                  <Button color='primary' variant='contained' onClick={onUsernameButtonClick} fullWidth>Submit</Button>
+                </>
+              )
+              : (
+                <>
+                  <SwipeableViews axis='x' index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents>
+                    {questions.map(question => (
+                      <div key={question.triviaQuestionId}>
+                        <Typography variant='h6' gutterBottom sx={{ textAlign: 'center' }}>{question.question}</Typography>
+                        <Button color='primary' variant='contained' sx={{ width: '50%' }} onClick={() => onHimHerButtonClick('Him', question.triviaQuestionId)}>Him</Button>
+                        <Button color='secondary' variant='contained' sx={{ width: '50%' }} onClick={() => onHimHerButtonClick('Her', question.triviaQuestionId)}>Her</Button>
+                      </div>
+                    ))}
+                  </SwipeableViews>
+                  <MobileStepper steps={questions.length} position="static" activeStep={activeStep}
+                    nextButton={
+                      <Button size="small" onClick={handleNext} disabled={activeStep === questions.length - 1}>
+                        Next
+                        <KeyboardArrowRight />
+                      </Button>
+                    }
+                    backButton={
+                      <Button size="small" onClick={handleBack} disabled={true}>
+                        <KeyboardArrowLeft />
+                        Back
+                      </Button>
+                    }
+                  />
+                </>
+              )
         }
       </Grid>
     </ListPageLayout>
